50ddecce12a0a698de53d498ba956f3a0cef58d3
Avic-online <avicdon1@gmail.com>
2024-01-28 19:40:52 +0100

    adding num
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -2,10 +2,8 @@
 // a javascript function to add number
 
 function add(number1, number2){
-    // let sum = number1 + number2
-    // let number1 = 3
-    // let number2 = 5
-    return number1 + number2
+    let sum = number1 + number2;
+    return sum;
 }
 
 console.log(add(3, 5));
