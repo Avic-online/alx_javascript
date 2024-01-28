15b7b1c89e43915b8b49b2e7ff7a27d80c4e0444
Avic-online <avicdon1@gmail.com>
2024-01-28 18:19:11 +0100

    replace
    Signed-off-by: Avic-online <avicdon1@gmail.com>

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// this code replaces a 12
+
+let myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+
+  myObject={type:'object', value:89}
+  
+  console.log(myObject);
