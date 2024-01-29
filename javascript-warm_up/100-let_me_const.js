09c3bd6b06a86c08fc02c39246408df9efa8c5e8
Avic-online <avicdon1@gmail.com>
2024-01-29 14:05:45 +0100

    change value
    Signed-off-by: Avic-online <avicdon1@gmail.com>

---
+++ javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// this program changes the value of the variable myVal to 333
+
+
+var myVar = 89;
+// require('./100-let_me_const')
+
+console.log(myVar)
+myVar = 333
+console.log(myVar)
