35fa83110d46ba3878b0b59c3cee6e870702e674
Avic-online <avicdon1@gmail.com>
2024-01-29 13:53:43 +0100

    okay
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 // a javascript function to add number
 
-function add(number1, number2){
-    const sum = number1 + number2;
-    return sum;
-}
+// function add(number1, number2){
+//     const sum = number1 + number2;
+//     return sum;
+// }
+
+exports.add = (a, b) => a + b;
 
 // console.log(add(3, 5));
-console.log(add(4, 85));
-console.log(add(93, -4));
-console.log(add(0, 89));
+// console.log(add(4, 85));
+// console.log(add(93, -4));
+// console.log(add(0, 89));
