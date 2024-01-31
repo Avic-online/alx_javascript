f67ae4f2e1ab4cbf50fb326efafd8ac48678e2ed
Avic-online <avicdon1@gmail.com>
2024-01-31 21:39:34 +0100

    okay if js
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,7 +4,7 @@
 
 module.exports = class Rectangle {
     constructor(w, h) {
-        if (w==0 || !Number.isInteger(w) && h==0 || !Number.isInteger(h)){
+        if (w==0 || !Number.isInteger(w) || h==0 || !Number.isInteger(h)){
             return{};
         };
         this.width = w;
