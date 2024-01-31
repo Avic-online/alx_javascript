982ce5d12966de8feb44099b0df116dadd46e33b
Avic-online <avicdon1@gmail.com>
2024-01-31 21:13:12 +0100

    okay, rectangle
    Signed-off-by: Avic-online <avicdon1@gmail.com>

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// a class that takes the width and height of a rectangle and initializes its instance
+
+module.exports = class Rectangle {
+    myFunction(w, h) {
+        this.width = w;
+        this.height = h;
+    }
+
+}
+
+const r1 = new Rectangle(2, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
+
+const r2 = new Rectangle(2, -3);
+console.log(r2);
+console.log(r2.width);
+console.log(r2.height);
+
+const r3 = new Rectangle(2);
+console.log(r3);
+console.log(r3.width);
+console.log(r3.height);
