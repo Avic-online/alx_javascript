1bd164d8126a45004f0e662a9583927466b1dbdb
Avic-online <avicdon1@gmail.com>
2024-01-31 21:33:39 +0100

    ok
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -7,7 +7,7 @@ module.exports = class Rectangle {
         this.width = w;
         this.height = h;
 
-        if (w==0 || !Number.isInteger(3) || h==0 || !Number.isInteger(h)){
+        if (w==0 || !Number.isInteger(3) && h==0 || !Number.isInteger(h)){
             return{};
         };
     };
@@ -15,23 +15,23 @@ module.exports = class Rectangle {
 };
 
 
-const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
+// const r1 = new Rectangle(2, 3);
+// console.log(r1);
+// console.log(r1.width);
+// console.log(r1.height);
 
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
+// const r2 = new Rectangle(2, -3);
+// console.log(r2);
+// console.log(r2.width);
+// console.log(r2.height);
 
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
+// const r3 = new Rectangle(2);
+// console.log(r3);
+// console.log(r3.width);
+// console.log(r3.height);
 
-const r4 = new Rectangle(2, 0);
-console.log(r4);
-console.log(r4.width);
-console.log(r4.height);
+// const r4 = new Rectangle(2, 0);
+// console.log(r4);
+// console.log(r4.width);
+// console.log(r4.height);
 
