8de02abc112e9c0d5ee146b0c15417bc58979ea5
Avic-online <avicdon1@gmail.com>
2024-02-01 20:34:24 +0100

    popop
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -13,15 +13,15 @@ module.exports = class Rectangle {
 };
 
 
-// const r1 = new Rectangle(2, 3);
-// console.log(r1);
-// console.log(r1.width);
-// console.log(r1.height);
+const r1 = new Rectangle(2, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
 
-// const r2 = new Rectangle(2, -3);
-// console.log(r2);
-// console.log(r2.width);
-// console.log(r2.height);
+const r2 = new Rectangle(2, -3);
+console.log(r2);
+console.log(r2.width);
+console.log(r2.height);
 
 // const r3 = new Rectangle(2);
 // console.log(r3);
