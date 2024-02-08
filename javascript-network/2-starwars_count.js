649142cd5edf4cf08e2b844a40566f43ea7ec77d
Avic-online <avicdon1@gmail.com>
2024-02-08 21:36:52 +0100

    star wars counts 18
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -5,9 +5,17 @@ const request = require('request')
 
 const id = process.argv[2]
 
-const endpoint = 'https://swapi-api.alx-tools.com/api/films/'+id
+const endpoint = 'https://swapi-api.alx-tools.com/api/films'
 
 request(endpoint, function(error, response, body) {
-    const ObjectCharacter = JSON.parse(body)
-    console.log(ObjectCharacter.title)
+    if (!error) {
+        const results = JSON.parse(body).results
+        console.log(results.reduce((count, movie) => {
+            return movie.characters.find((character) => character.endsWith('/18/'))
+                ? count + 1
+                :count
+        }, 0))
+    }
+    // const ObjectCharacter = JSON.parse(body)
+    // console.log(ObjectCharacter.title)
 })
