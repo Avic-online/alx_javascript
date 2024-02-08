9604943b22b49ffbc5117f23aa2c48d942e47e29
Avic-online <avicdon1@gmail.com>
2024-02-08 22:22:09 +0100

    okay
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // this program fetches and store the content of a web page
 
-const saveFile = require('saveFile')
+const fs = require('fs')
 const request = require('request')
 request(process.argv[2])
-  .pipe(saveFile.createWriteStream(process.argv[3]))
+  .pipe(fs.createWriteStream(process.argv[3]))
