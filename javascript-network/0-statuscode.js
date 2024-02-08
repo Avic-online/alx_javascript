7845ec11fdc0b68e4b08ea7e5008ef0f6cf011dc
Avic-online <avicdon1@gmail.com>
2024-02-08 21:20:43 +0100

    status code
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -2,8 +2,15 @@
 // javascript program to get status code of a url request
 
 
-const request = require('request')
-const url = 'https://developer.mozilla.org/en-US/'
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/2';
 request.get(url, function(error, response, body) {
+    // const urlObject = JSON.parse(response)
     console.log('code:', response.statusCode)
-})
+});
+
+// const request = require('request')
+// request.get(process.argv[2])
+//   .on('response', function(response) {
+//     console.log('code:', response.statusCode)
+//   })
